0f7f21667f6eba81d3f7b8985924306467e2ded1
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-04 16:57:49 +0200

    0x03 - Tasks (3-5)

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(students) {
+  if (Array.isArray(students)) {
+    return students.reduce((accumulator, each) => accumulator + each.id, 0);
+  }
+
+  return [];
+}
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
+      return {
+        ...student,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
+}
