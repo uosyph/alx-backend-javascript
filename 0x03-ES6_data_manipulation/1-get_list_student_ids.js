f011623bcf49f9bf0bfe4cf6fe48a9c744b3a47e
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-04 16:50:10 +0200

    0x03 - Tasks (0-2)

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(students) {
+  if (Array.isArray(students)) {
+    return students.map((items) => items.id);
+  }
+
+  return [];
+}
---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(students, city) {
+  if (Array.isArray(students)) {
+    return students.filter((items) => items.location === city);
+  }
+
+  return [];
+}
