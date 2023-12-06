22b30d361812d568d9bdb0972732b8753f0f4f0a
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-06 16:27:25 +0200

    0x04 - Task (1-3)

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,26 @@
+// Task 1: Teacher interface
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+  [propName: string]: any;
+}
+
+// Task 2: Directors interface that extends Teacher
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Task 3: Printing teachers
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  return `${firstName[0]}. ${lastName}`;
+};
