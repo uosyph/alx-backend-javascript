bf5ddeee97410dc829bfd13ea6d6c5acf1c8ea24
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-06 16:27:52 +0200

    0x04 - Task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,3 +24,27 @@ const printTeacher: printTeacherFunction = (
 ): string => {
   return `${firstName[0]}. ${lastName}`;
 };
+
+// Task 4: Writing a class
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() { return 'Currently working'; }
+  displayName() { return this.firstName; }
+};
+
+export interface StudentClassInterface {
+  readonly firstName: string;
+  readonly lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
