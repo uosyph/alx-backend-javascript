e78b7f9db3de71f22e7de95d4d87a1a97a4a93de
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-06 15:39:46 +0200

    0x03 - 8: Handle undefined elements

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
 
   return [...set]
-    .filter((el) => el.startsWith(startString))
+    .filter((el) => el !== undefined && el.startsWith(startString))
     .map((el) => el.slice(startString.length))
     .join('-');
 }
