502816e3e8708f82857ea50b7776a8f6a020f370
Yousef Saeed <yousafesaeed@gmail.com>
2023-12-06 16:49:37 +0200

    0x04 - Task (6,7)

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -23,3 +23,19 @@ export class Teacher implements TeacherInterface {
 }
 
 export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
+
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    let job = undefined;
+    (isDirector(employee)) ? job = employee.workDirectorTasks() : job = employee.workTeacherTasks();
+    return job;
+}
+
+type Subjects = "Math" | "History";
+export function teachClass(todayClass: Subjects): string {
+    if (todayClass === "Math") { return "Teaching Math"; }
+    else if (todayClass === "History") { return "Teaching History"; }
+}
